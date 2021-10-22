5800c654d631b7b094d55ad0044a701828594760
Vanshal Gaur <43922960+Vanshal@users.noreply.github.com>
2021-10-22 20:20:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,38 @@
-alert(1);
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+document.write ("This is remote text via xss.js located at ha.ckers.org " + document.cookie);
+alert ("This is remote text via xss.js located at ha.ckers.org " + document.cookie);
+
+
+}
+/*
+     FILE ARCHIVED ON 17:22:42 Mar 17, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:49:24 Oct 22, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  cdx.remote: 0.121
+  LoadShardBlock: 442.564 (3)
+  PetaboxLoader3.resolve: 209.278 (2)
+  captures_list: 537.002
+  exclusion.robots.policy: 0.207
+  esindex: 0.015
+  CDXLines.iter: 20.584 (3)
+  exclusion.robots: 0.222
+  load_resource: 692.477 (2)
+  PetaboxLoader3.datanode: 623.354 (5)
+*/
