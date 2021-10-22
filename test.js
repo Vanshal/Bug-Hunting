3fac1461f9fdfaa889afc112a8493568ffd32451
Vanshal Gaur <43922960+Vanshal@users.noreply.github.com>
2021-10-22 20:21:38 +0530

    Update test.js

--- test.js
+++ test.js
@@ -15,24 +15,3 @@ alert ("This is remote text via xss.js located at ha.ckers.org " + document.cook
 
 
 }
-/*
-     FILE ARCHIVED ON 17:22:42 Mar 17, 2012 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 14:49:24 Oct 22, 2021.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  cdx.remote: 0.121
-  LoadShardBlock: 442.564 (3)
-  PetaboxLoader3.resolve: 209.278 (2)
-  captures_list: 537.002
-  exclusion.robots.policy: 0.207
-  esindex: 0.015
-  CDXLines.iter: 20.584 (3)
-  exclusion.robots: 0.222
-  load_resource: 692.477 (2)
-  PetaboxLoader3.datanode: 623.354 (5)
-*/
